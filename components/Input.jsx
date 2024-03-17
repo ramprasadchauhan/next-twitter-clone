6cd5ea01a08e33ec8da115a0f143e23ce64faab1
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-03-17 18:38:17 +0530

    send data to firebase and add loading effect

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,13 +1,67 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { FaceSmileIcon, PhotoIcon } from "@heroicons/react/24/outline";
+import {
+  FaceSmileIcon,
+  PhotoIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { SessionProvider, useSession, signOut } from "next-auth/react";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { db, storage } from "@/firebase";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 const Input = () => {
   const { data: session } = useSession();
-  console.log(session);
+  const [input, setInput] = useState("");
+  const filePickerRef = useRef();
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const sendPost = async () => {
+    if (loading) return;
+    setLoading(true);
+    const docRef = await addDoc(collection(db, "posts"), {
+      id: session.user.uid,
+      text: input,
+      userImg: session.user.image,
+      timestamp: serverTimestamp(),
+      name: session.user.name,
+      username: session.user.username,
+    });
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+    if (selectedFile) {
+      await uploadString(imageRef, selectedFile, "data_url").then(async () => {
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, "posts", docRef.id), {
+          image: downloadURL,
+        });
+      });
+    }
+    setInput("");
+    setSelectedFile(null);
+    setLoading(false);
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    reader.onload = (readerEvent) => {
+      console.log(readerEvent.target.result);
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
+
   return (
     <>
       {session && (
@@ -27,16 +81,47 @@ const Input = () => {
                 className="border-none w-full focus:ring-0 text-lg placeholder-gray-700 text-gray-700 tracking-wide min-h-[50px] "
                 rows="2"
                 placeholder="What's happening?"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
               ></textarea>
             </div>
-            <div className="flex items-center justify-between pt-2.5">
-              <div className="flex gap-1">
-                <PhotoIcon className="h-10 text-sky-500 hovetEffect w-10 bg-sky-100 rounded-full" />
-                <FaceSmileIcon className="h-10 text-sky-500 hovetEffect w-10 bg-sky-100 rounded-full" />
+            {selectedFile && (
+              <div className="relative">
+                <XMarkIcon
+                  onClick={() => setSelectedFile(null)}
+                  className="h-7 text-black hover:text-red-700 border rounded-full shadow-md shadow-white transition ease-in-out border-red-600 hover:bg-yellow-400 absolute cursor-pointer"
+                />
+                <img
+                  className={`${loading && "animate-pulse"}`}
+                  src={selectedFile}
+                  alt="file"
+                />
               </div>
-              <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">
-                Tweet
-              </button>
+            )}
+            <div className="flex items-center justify-between pt-2.5">
+              {!loading && (
+                <>
+                  <div className="flex gap-1">
+                    <div onClick={() => filePickerRef.current.click()}>
+                      <PhotoIcon className="h-10 text-sky-500 hovetEffect w-10 bg-sky-100 rounded-full" />
+                      <input
+                        type="file"
+                        hidden
+                        ref={filePickerRef}
+                        onChange={addImageToPost}
+                      />
+                    </div>
+                    <FaceSmileIcon className="h-10 text-sky-500 hovetEffect w-10 bg-sky-100 rounded-full" />
+                  </div>
+                  <button
+                    onClick={sendPost}
+                    disabled={!input.trim()}
+                    className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
+                  >
+                    Tweet
+                  </button>
+                </>
+              )}
             </div>
           </div>
         </div>
